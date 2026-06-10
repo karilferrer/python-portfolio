9d19f21bffe612d21fae5bba24797e1d811de5db
karilferrer <152678084+karilferrer@users.noreply.github.com>
2026-06-10 11:16:21 +0800

    Delete CLI/practice.py

--- CLI/practice.py
+++
@@ -1,9 +0,0 @@
-def most_frequent_word_optimized(sentence):
-    words = sentence.lower().split()
-    
-    word_counts = {}
-    for word in words:
-        word_counts[word] = word_counts.get(word, 0) + 1 # A cleaner way to build the dict
-        
-    # Use max() on the dictionary keys, looking at their values (counts) to find the max
-    return max(word_counts, key=word_counts.get)
